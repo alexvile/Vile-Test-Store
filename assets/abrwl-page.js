3d29d05b0d1aeea794ba744f5164e6867e5e9935
Oleksii Solotvinskyi <Oleksii.Solotvinskyi@abrio.de>
2025-01-23 17:27:31 +0200

    feat: last changes

--- assets/abrwl-page.js
+++ assets/abrwl-page.js
@@ -1,10 +1,15 @@
 (function () {
   class WishlistPage {
     constructor() {
-      this.asdasd = "asadas";
+      this.storageKey = "abrio_wl_data";
+      this.cacheKey = "abrio_wl_cache";
+      this.lists = [];
+      this.init();
     }
+
+    init() {}
   }
 
   const mm = new WishlistPage();
   window.wp = mm;
-});
+})();
